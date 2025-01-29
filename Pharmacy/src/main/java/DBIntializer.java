8393b4549d661bbf022bc2e1a5e74ac9af73cc16
Akash Kadam <146556682+akashkadam04@users.noreply.github.com>
2025-01-29 15:08:05 +0530

    Delete Pharmacy directory

--- Pharmacy/src/main/java/DBIntializer.java
+++
@@ -1,8 +0,0 @@
-
-public interface DBIntializer {
-String DRIVER="com.mysql.cj.jdbc.Driver";
-String CON_STRING="jdbc:mysql://localhost:3306/pharmacy";
-String USERNAME="root";
-String PASSWORD="root";
-}
-
